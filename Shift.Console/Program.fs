715c2ffe11947ad1a766030113114af2704f6299
Jerome Gamo <gamo.jerome@icloud.com>
2019-01-09 12:47:29 +0800

    Add inifinite loop of accepting console inputs with loop termination

--- Shift.Console/Program.fs
+++ Shift.Console/Program.fs
@@ -1,8 +1,12 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let run () =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun cmd -> cmd <> "exit")
+    |> Seq.iter (fun cmd -> printfn "Input: %A" cmd)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    run()
+    printfn "Exited"
+    0
